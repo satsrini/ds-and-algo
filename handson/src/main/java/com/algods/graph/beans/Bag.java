083d454eb39cfd8f4185650b25f8cfaaa06865e6
root <root@hash.us-west4-b.c.valid-dragon-326216.internal>
2021-09-20 15:11:27 +0000

    Full Bag implementation with Iterator

--- handson/src/main/java/com/algods/graph/beans/Bag.java
+++ handson/src/main/java/com/algods/graph/beans/Bag.java
@@ -2,17 +2,19 @@ package com.algods.graph.beans;
 
 
 /**
-* <h1>Bag</h1>
-* This class is an implementation for Bag Data Structure
-* <p> Implementation for Bag Data Structure in Java
-* 
-*
-* @author  Satish Srinivasan
-* @version 1.0
-* @since   2021-09-20
-*/
+  * <h1>Bag</h1>
+  * This class is an implementation for Bag Data Structure
+  * <p> Implementation for Bag Data Structure in Java
+  * 
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-09-20
+  */
 
-public class Bag<Item>
+import java.util.Iterator;
+
+public class Bag<Item> implements Iterable<Item>
 {
 
    private Node first;
@@ -24,7 +26,6 @@ public class Bag<Item>
       first = newNode;
    }
 
-
    private class Node
    {
       private Item item;
@@ -33,8 +34,36 @@ public class Bag<Item>
       public Node(Item item)
       {
          this.item = item;
-
       }
+   }
+
+   public Iterator<Item> iterator()
+   {
+      return new BagIterator();
+   }
+
+   private class BagIterator implements Iterator<Item>
+   {
+
+      private Node currentNode;
+
+      public BagIterator()
+      {
+         currentNode = first;
+      }
+
+      public boolean hasNext()
+      {
+         return (currentNode != null);
+      }
+
+      public Item next()
+      {
+         Item nextItem = currentNode.item;
+         currentNode = currentNode.next;
+         return nextItem;
+      }
+
    }
  
 }
