de77fe69badfdff5320dd661b8586fd4816a24a9
root <root@myvm2.asia-south1-c.c.feisty-mechanic-327015.internal>
2021-10-12 15:35:22 +0000

    Skeleton implementation for DiGraph

---
+++ handson/src/main/java/com/algods/graph/directed/DiGraph.java
@@ -0,0 +1,42 @@
+package com.algods.graph.directed;
+
+public class DiGraph
+{
+
+    public DiGraph(int V)
+    {
+
+    }
+
+    public int V()
+    {
+       return -1;
+    }
+
+    public int E()
+    {
+       return -1;
+    }
+
+    public void addEdge(int v, int w)
+    {
+
+    }
+
+    public Iterable<Integer> adj(int v)
+    {
+        return null;
+    }
+
+    public DiGraph reverse()
+    {
+        return null;
+    }
+
+    @Override
+    public String toString()
+    {
+       return super.toString();
+    }
+
+}
