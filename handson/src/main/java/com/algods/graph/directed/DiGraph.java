145823ef6a21754e5d8246761a5f4da4de3448cd
root <root@myvm2.asia-south1-c.c.feisty-mechanic-327015.internal>
2021-10-13 17:28:24 +0000

    Complete Implementation for DiGraph

--- handson/src/main/java/com/algods/graph/directed/DiGraph.java
+++ handson/src/main/java/com/algods/graph/directed/DiGraph.java
@@ -1,6 +1,6 @@
 package com.algods.graph.directed;
 
-
+import com.algods.graph.beans.Bag;
 
 /**
   * <h1>DiGraph</h1>
@@ -17,40 +17,81 @@ package com.algods.graph.directed;
 public class DiGraph
 {
 
+    private int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
     public DiGraph(int V)
     {
+       this.V = V;
+       this.E = 0;
+       adj = (Bag<Integer>[])new Bag[this.V];
 
+       for(int i = 0; i < V; i++)
+       {
+          adj[i] = new Bag<Integer>();
+       }
     }
 
     public int V()
     {
-       return -1;
+       return V;
     }
 
     public int E()
     {
-       return -1;
+       return E;
     }
 
     public void addEdge(int v, int w)
     {
 
+       if(v >= V || w >= V)
+       {
+           String msg = String.format("Input vertex cannot be greater than the max vertex index %d", (V-1));
+           throw new RuntimeException(msg);
+       }
+
+       adj[v].add(w);
+       this.E++;
     }
 
     public Iterable<Integer> adj(int v)
     {
-        return null;
+        return adj[v];
     }
 
     public DiGraph reverse()
     {
-        return null;
+        DiGraph diGraph = new DiGraph(this.V);
+        
+        for(int i = 0; i < V; i++)
+        {
+           for(int k:this.adj(i))
+           {
+               diGraph.addEdge(k,i);
+           }            
+        }
+
+        return diGraph;
     }
 
     @Override
     public String toString()
     {
-       return super.toString();
+       StringBuffer result = new StringBuffer();
+
+       for(int i = 0; i < V; i++)
+       {
+         result.append(i + ": ");
+         for(int k:this.adj(i))
+         {
+            result.append(k + " ");
+         }
+         result.append("\n");
+       }
+
+       return result.toString();
     }
 
 }
