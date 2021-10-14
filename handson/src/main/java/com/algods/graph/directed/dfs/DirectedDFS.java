c2b69eb454175beaf1ba62655fe3415adf178e9f
root <root@myvm2.asia-south1-c.c.feisty-mechanic-327015.internal>
2021-10-14 09:44:44 +0000

    Skeleton class for DirectedDFS

---
+++ handson/src/main/java/com/algods/graph/directed/dfs/DirectedDFS.java
@@ -0,0 +1,25 @@
+package com.algods.graph.directed.dfs;
+
+import com.algods.graph.directed.DiGraph;
+
+public class DirectedDFS
+{
+
+    public DirectedDFS(DiGraph g, int s)
+    {
+
+
+    }
+
+    public DirectedDFS(DiGraph g, Iterable<Integer> sources)
+    {
+
+    }
+
+    public boolean marked(int v)
+    {
+       return false;
+    }
+
+
+}
