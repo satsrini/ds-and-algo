520de726c8b943eaf14cacd08eb1a0ea161cf6c2
root <root@myvm2.asia-south1-c.c.feisty-mechanic-327015.internal>
2021-10-14 17:18:42 +0000

    Implementation for Depth First Path for Directed Graph

--- handson/src/main/java/com/algods/graph/directed/dfs/path/DepthFirstDirectedPaths.java
+++ handson/src/main/java/com/algods/graph/directed/dfs/path/DepthFirstDirectedPaths.java
@@ -18,17 +18,42 @@ import com.algods.graph.directed.DiGraph;
 
 public class DepthFirstDirectedPaths
 {
+     private boolean[] marked;
+     private int[] edgeTo;
+     private int s;
 
      public DepthFirstDirectedPaths(DiGraph g, int s)
      {
+         this.s = s;
+         marked = new boolean[g.V()];
+         edgeTo = new int[g.V()];
 
+         dfs(g,s);
+     }
 
+     private void dfs(DiGraph g, int v)
+     {
+        marked[v] = true;
+
+        for(int w:g.adj(v))
+        {
+           if(!marked[w])
+           {
+              edgeTo[w] = v;
+              dfs(g,w);
+           }
+        }
      }
 
 
      public boolean hasPathTo(int v)
      {
-        return false;
+        if(v < 0 || v >= marked.length)
+        {
+           throw new RuntimeException("Input index is outside the DiGraph bounds");
+        }
+
+        return marked[v];
      }
 
      public Iterable<Integer> pathTo(int v)
