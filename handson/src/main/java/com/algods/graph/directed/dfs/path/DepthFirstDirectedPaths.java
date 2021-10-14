015f4b563e412cfd4b6d15b880aad62d69cde19d
root <root@myvm2.asia-south1-c.c.feisty-mechanic-327015.internal>
2021-10-14 16:56:23 +0000

    Skeleton for DepthFirstDirectedPaths

---
+++ handson/src/main/java/com/algods/graph/directed/dfs/path/DepthFirstDirectedPaths.java
@@ -0,0 +1,40 @@
+package com.algods.graph.directed.dfs.path;
+
+
+import com.algods.graph.directed.DiGraph;
+
+/**
+  * <h1>DepthFirstDirectedPaths</h1>
+  * This class is an implementation for DepthFirstDirectedPaths algorithm
+  * <p> Implementation for finding path between vertices in a directed
+  * graph using Depth First Path algorithm in Java
+  *
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-10-14
+  */
+
+
+public class DepthFirstDirectedPaths
+{
+
+     public DepthFirstDirectedPaths(DiGraph g, int s)
+     {
+
+
+     }
+
+
+     public boolean hasPathTo(int v)
+     {
+        return false;
+     }
+
+     public Iterable<Integer> pathTo(int v)
+     {
+        return null;
+
+     }
+
+}
