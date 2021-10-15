7b87b7f582589b0d6bd71f72cecff101dd68c491
satsrini <satsrinister@gmail.com>
2021-10-15 11:33:24 +0000

    Skeleton implementation for BreadFirstDirectedPaths

---
+++ handson/src/main/java/com/algods/graph/directed/dfs/path/BreadthFirstDirectedPaths.java
@@ -0,0 +1,38 @@
+package com.algods.graph.directed.dfs.path;
+
+import com.algods.graph.directed.DiGraph;
+
+/**
+  * <h1>BreadthFirstDirectedPaths</h1>
+  * This class is an implementation for BreadthFirstDirectedPaths algorithm
+  * <p> Implementation for finding path between vertices in a directed
+  * graph using Breadth First Path algorithm in Java
+  *
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-10-15
+  */
+
+
+public class BreadthFirstDirectedPaths
+{
+
+
+    public BreadthFirstDirectedPaths(DiGraph g, int s)
+    {
+
+
+    }
+
+    public boolean hasPathTo(int v)
+    {
+         return false;
+    }
+
+    public Iterable<Integer> pathTo(int v)
+    {
+         return null;
+    }
+
+}
