261298e4bdf6a1d3bbd47f7dfe9195d37714be58
satsrini <satsrinister@gmail.com>
2021-10-16 15:53:56 +0000

    Skeleton implementation for DirectedCycle

---
+++ handson/src/main/java/com/algods/graph/directed/dfs/dag/DirectedCycle.java
@@ -0,0 +1,23 @@
+package com.algods.graph.directed.dfs.dag;
+
+
+
+/**
+  * <h1>DirectedCycle</h1>
+  * This class is an implementation for DirectedCycle algorithm
+  * <p> Implementation for finding if a given DiGraph is a Directed Acyclic Graph(DAG)
+  * or not. The algorithm checks if there is atleast one cycle present in the DiGraph.
+  * Also the algorithm finds the path of the cycle present.
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-10-16
+  */
+
+
+public class DirectedCycle
+{
+
+
+
+}
