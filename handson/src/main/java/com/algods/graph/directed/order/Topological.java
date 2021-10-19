a889132217339ce3dc9c82cf3c911057dc4d46be
satsrini <satsrinister@gmail.com>
2021-10-19 08:40:46 +0000

    Skeleton implementation for Topological sort

---
+++ handson/src/main/java/com/algods/graph/directed/order/Topological.java
@@ -0,0 +1,36 @@
+package com.algods.graph.directed.order;
+
+
+
+/**
+  * <h1>Topological</h1>
+  * This class is an implementation for Topologocal sort algorithm
+  * <p> Implementation for finding Topological orders in a DAG(Directed Acyclic Graph).
+  * This class gives the Topological sort order for a given DiGraph
+  *
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-10-18
+  */
+
+
+public class Topological
+{
+
+    public Topological()
+    {
+
+    }
+
+    public Iterable<Integer> order()
+    {
+       return null;
+    }
+
+    public boolean isDAG()
+    {
+       return false;
+    }
+
+}
