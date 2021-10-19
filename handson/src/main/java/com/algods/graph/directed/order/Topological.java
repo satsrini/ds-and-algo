83e64017863e5e4b68d75255f696161839e3ac06
satsrini <satsrinister@gmail.com>
2021-10-19 08:53:44 +0000

    Full Implementation of Topological sort for a DiGraph

--- handson/src/main/java/com/algods/graph/directed/order/Topological.java
+++ handson/src/main/java/com/algods/graph/directed/order/Topological.java
@@ -1,6 +1,7 @@
 package com.algods.graph.directed.order;
 
-
+import com.algods.graph.directed.DiGraph;
+import com.algods.graph.directed.dfs.dag.DirectedCycle;
 
 /**
   * <h1>Topological</h1>
@@ -11,26 +12,39 @@ package com.algods.graph.directed.order;
   *
   * @author  Satish Srinivasan
   * @version 1.0
-  * @since   2021-10-18
+  * @since   2021-10-19
   */
 
 
 public class Topological
 {
 
-    public Topological()
+    private Iterable<Integer> order;
+    private boolean cycle;
+
+    public Topological(DiGraph g)
     {
+       DirectedCycle directedCycle = new DirectedCycle(g);
+  
+       cycle = directedCycle.hasCycle();
+
+       if(!cycle)
+       {
+           DepthFirstOrder depthFirstOrder = new DepthFirstOrder(g);
+           order = depthFirstOrder.reversePost();
+         
+       }
 
     }
 
     public Iterable<Integer> order()
     {
-       return null;
+       return order;
     }
 
     public boolean isDAG()
     {
-       return false;
+       return cycle;
     }
 
 }
