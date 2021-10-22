bc526b9664ebc57aaa1992891947f9fc6f6ef8b8
satsrini <satsrinister@gmail.com>
2021-10-22 14:53:07 +0000

    More test cases for DepthFirst Order

--- handson/src/test/java/com/algods/graph/directed/order/TestDepthFirstOrder.java
+++ handson/src/test/java/com/algods/graph/directed/order/TestDepthFirstOrder.java
@@ -15,6 +15,7 @@ import java.util.List;
 public class TestDepthFirstOrder
 {
 	private DiGraph g;
+        private DiGraph g2;
 	
 
 	@Before
@@ -37,6 +38,30 @@ public class TestDepthFirstOrder
                 g.addEdge(9,11);
                 g.addEdge(9,12);
                 g.addEdge(11,12);
+
+                g2 = new DiGraph(13);
+                g2.addEdge(0,1);
+                g2.addEdge(0,5);
+                g2.addEdge(2,0);
+                g2.addEdge(2,3);
+                g2.addEdge(3,2);
+                g2.addEdge(3,5);
+                g2.addEdge(4,2);
+                g2.addEdge(4,3);
+                g2.addEdge(5,4);
+                g2.addEdge(6,0);
+                g2.addEdge(6,4);            
+                g2.addEdge(6,9);
+                g2.addEdge(7,6);
+                g2.addEdge(7,8);
+                g2.addEdge(8,7);
+                g2.addEdge(8,9);
+                g2.addEdge(9,10);
+                g2.addEdge(9,11);
+                g2.addEdge(10,12);
+                g2.addEdge(11,4);
+                g2.addEdge(11,12);
+                g2.addEdge(12,9);
 	}
 	
         @Test
@@ -92,6 +117,49 @@ public class TestDepthFirstOrder
            }
 
            System.out.println("\n\n");
+
+           assertEquals(true,true);
+
+        }
+
+        @Test
+        public void testPrintAllThree()
+        {
+           DepthFirstOrder depthFirstOrder = new DepthFirstOrder(g2.reverse());
+
+           System.out.println("\n\n");
+           System.out.println("PRE");
+       
+           for(int k:depthFirstOrder.pre())
+           {
+              System.out.println(k + " ");
+           }
+
+           System.out.println("\n\n");
+
+
+           System.out.println("\n\n");
+           System.out.println("POST");
+
+           for(int l:depthFirstOrder.post())
+           {
+              System.out.println(l + " ");
+           }
+
+           System.out.println("\n\n");
+
+
+           System.out.println("\n\n");
+           System.out.println("REVERSE POST");
+
+           for(int m:depthFirstOrder.reversePost())
+           {
+              System.out.println(m + " ");
+           }
+
+           System.out.println("\n\n");
+
+
 
            assertEquals(true,true);
 
