3d1f288b63b04660a6de5137a29a05603cd4d63b
satsrini <satsrinister@gmail.com>
2021-10-22 15:30:35 +0000

    Full implementation for KosarajuSCC

---
+++ handson/src/main/java/com/algods/graph/directed/scc/KosarajuSCC.java
@@ -0,0 +1,79 @@
+package com.algods.graph.directed.scc;
+
+import com.algods.graph.directed.DiGraph;
+import com.algods.graph.directed.order.DepthFirstOrder;
+
+/**
+  * <h1>KosarajuSCC</h1>
+  * This class is an implementation for Kosaraju's Strongly Connected Components in a 
+  * DiGraph
+  * <p> Implementation for finding Strongly Connected components in a DiGraph.
+  *     It uses Kosaraju's algorithm making use of DepthFirstOrder approach.
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-10-22
+  */
+
+
+public class KosarajuSCC
+{
+
+    private int count;
+    private int[] id;
+    private boolean[] marked;
+
+    public KosarajuSCC(DiGraph g)
+    {
+        id = new int[g.V()];
+        marked = new boolean[g.V()];
+
+        DepthFirstOrder depthFirstOrder = new DepthFirstOrder(g.reverse());
+
+        for(int s:depthFirstOrder.reversePost())
+        {
+           dfs(g,s);
+        }
+
+    }
+
+    private void dfs(DiGraph g, int v)
+    {
+        id[v] = count;
+        marked[v] = true;
+
+        for(int k:g.adj(v))
+        {
+           if(!marked[v])
+           {
+              dfs(g,k);
+           }
+        }
+    }
+
+    public boolean stronglyConnected(int v, int w)
+    {
+ 
+       if(v < 0 || w < 0 || v >= id.length || w >= id.length)
+       {
+          throw new RuntimeException("Invalid input");
+       }
+
+       return (id[v] == id[w]);
+    }
+
+    public int count()
+    {
+       return count;
+    }
+
+    public int id(int v)
+    {
+       if(v >= id.length || v < 0)
+       {
+           throw new RuntimeException("Invalid input");
+       }
+       return id[v];
+    }
+
+}
