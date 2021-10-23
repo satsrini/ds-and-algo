2241fce57db758dde2e913eb7e4aacee5e603b70
satsrini <satsrinister@gmail.com>
2021-10-23 08:21:34 +0000

    Skeleton implementation for TransitiveClosure Algorithm in Java

---
+++ handson/src/main/java/com/algods/graph/directed/scc/TransitiveClosure.java
@@ -0,0 +1,31 @@
+package com.algods.graph.directed.scc;
+
+import com.algods.graph.directed.DiGraph;
+
+/**
+  * <h1>TransitiveClosure</h1>
+  * This class is an implementation for TransitiveClosure algorithm
+  * <p> Implementation for TransitiveClosure algorithm in Java
+  *
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-10-23
+  */
+
+
+
+public class TransitiveClosure
+{
+
+    public TransitiveClosure(DiGraph g)
+    {
+
+
+    }
+
+    public boolean reachable(int v, int w)
+    {
+       return false;
+    }
+}
