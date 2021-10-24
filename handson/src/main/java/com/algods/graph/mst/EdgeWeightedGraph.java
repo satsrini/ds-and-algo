eabd369cc56b10aeaacd4d8b5734bdea68931f5a
satsrini <satsrinister@gmail.com>
2021-10-24 05:20:17 +0000

    Skeleton implementation for EdgeWeightedGraph datastructure.

---
+++ handson/src/main/java/com/algods/graph/mst/EdgeWeightedGraph.java
@@ -0,0 +1,25 @@
+package com.algods.graph.mst;
+
+/**
+  * <h1>EdgeWeightedGraph</h1>
+  * This class is an implementation for EdgeWeightedGraph
+  * <p> Implementation for EdgeWeightedGraph Data Structure in Java to be used in
+  * Minimum Spanning Tree.
+  *
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-10-24
+  */
+
+
+public class EdgeWeightedGraph
+{
+
+     public EdgeWeightedGraph()
+     {
+
+     }
+
+
+}
