1516913c392f35164b1701785857a8cab99ecd35
satsrini <satsrinister@gmail.com>
2021-10-27 08:10:27 +0000

    Skeleton implementation for Binary Search Tree

---
+++ handson/src/main/java/com/algods/search/binarysearch/BST.java
@@ -0,0 +1,24 @@
+package com.algods.search.binarysearch;
+
+/**
+  * <h1>BST</h1>
+  * This class is an implementation for Binary Search Tree algorithm
+  * <p> Implementation for Binary Search Tree algorithm in Java to be used 
+  * to find inorder, preorder and postorder traversals
+  *
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-10-27
+  */
+
+
+public class BST
+{
+
+   public BST()
+   {
+
+   }
+
+}
