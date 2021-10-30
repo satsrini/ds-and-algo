50bcf62aaaa19581bdf5e6f779579b4722c5ee5b
satsrini <satsrinister@gmail.com>
2021-10-30 17:21:09 +0000

    sink() and swim() operations implementation for Minimum Priority Queue

--- handson/src/main/java/com/algods/sort/priorityqueue/MinPQ.java
+++ handson/src/main/java/com/algods/sort/priorityqueue/MinPQ.java
@@ -54,4 +54,36 @@ public class MinPQ<Key extends Comparable<Key>>
        pq[b] = temp;
     }
 
+    public void swim(int k)
+    {
+       while(k > 1 && less(k,k/2))
+       {
+          exch(k,k/2);
+          k = k/2;
+       }
+    }
+
+    public void sink(int k)
+    {
+       while(2*k <= N)
+       {
+         int j = 2*k;
+
+         if(j < N && !less(j,j+1))
+         {
+           j++;
+         }
+
+         if(less(k,j))
+         {
+            break;
+         }
+
+         exch(k,j);
+         k = j;
+
+       }
+
+    }
+
 }
