4788d606a610faac6bb96fc4227a1e986a0bb890
satsrini <satsrinister@gmail.com>
2021-10-30 08:27:05 +0000

    Adding skeleton unit test for MaxPQ

---
+++ handson/src/test/java/com/algods/sort/priorityqueue/TestMaxPQ.java
@@ -0,0 +1,22 @@
+package com.algods.sort.priorityqueue;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TestMaxPQ
+{
+
+	
+
+	@Before
+	public void setUp()
+	{
+
+        }
+
+}
