dd2202058774a069cc53543508af83efccac6d8b
satsrini <satsrinister@gmail.com>
2021-10-30 08:20:08 +0000

    sink and swim implementations of for Maximum Priority Queue using heap data structure

--- handson/src/main/java/com/algods/sort/priorityqueue/MaxPQ.java
+++ handson/src/main/java/com/algods/sort/priorityqueue/MaxPQ.java
@@ -56,4 +56,38 @@ public class MaxPQ<Key extends Comparable<Key>>
       pq[a] = pq[b];
       pq[b] = temp;
    }
+
+   private void sink(int k)
+   {
+
+      while(2*k <= N)
+      {
+         int j = 2*k;
+
+         if(j < N && less(j,j+1))
+         {
+             j++;
+         }
+
+         if(!less(k,j))
+         {
+            break;
+         }
+        
+         exch(k,j);
+         k = j;
+
+      }
+
+   }
+
+   private void swim(int k)
+   {
+      while(k > 1 && less(k/2,k))
+      {
+         exch(k,k/2);
+         k = k/2;
+      }
+   }
+   
 }
