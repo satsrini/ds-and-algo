b12840f1bc3327959339be2e543301261829dd90
satsrini <satsrinister@gmail.com>
2021-10-31 07:32:53 +0000

    Skeleton implementation for LazyPrimMST

---
+++ handson/src/main/java/com/algods/graph/mst/LazyPrimMST.java
@@ -0,0 +1,27 @@
+package com.algods.graph.mst;
+
+/**
+  * <h1>LazyPrimMST</h1>
+  * This class is an implementation for LazyPrimMST algorithm.
+  * <p> Implementation for Prim's Minimum Spanning Tree for an Edge Weighted Undirected 
+  * Graph. This approach uses Minimum Priority Queue of Edges with a 'lazy' approach.
+  * This Minimum Priority Queue is built using Binary Heap data structure.
+  *
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-10-31
+  */
+
+
+public class LazyPrimMST
+{
+
+    public LazyPrimMST()
+    {
+
+    }
+
+
+
+}
