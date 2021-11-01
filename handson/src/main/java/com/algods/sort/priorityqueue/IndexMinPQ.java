d13b9705d2fde8cf51bd4133c9172790a00c974c
satsrini <satsrinister@gmail.com>
2021-11-01 07:25:01 +0000

    Skeleton implementation for Index Minimum Priority Queue

---
+++ handson/src/main/java/com/algods/sort/priorityqueue/IndexMinPQ.java
@@ -0,0 +1,23 @@
+package com.algods.sort.priorityqueue;
+
+/**
+  * <h1>IndexMinPQ</h1>
+  * This class is an implementation for Index Minimum Priority Queue algorithm
+  * <p> Implementation of Minimum Priority Queue algorithm, with indices for each Key,
+  * using Binary Heap datastructure.
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-11-01
+  */
+
+
+public class IndexMinPQ<Key extends<Comparable<Key>>>
+{
+
+     public IndexMinPQ(int maxN)
+     {
+
+     }
+
+}
