53b72f0a990035026b015f34e9957a8c1c0a6385
satsrini <satsrinister@gmail.com>
2021-11-01 08:33:33 +0000

    implementation of less and exch for Index Min Priority Queue

--- handson/src/main/java/com/algods/sort/priorityqueue/IndexMinPQ.java
+++ handson/src/main/java/com/algods/sort/priorityqueue/IndexMinPQ.java
@@ -15,8 +15,68 @@ package com.algods.sort.priorityqueue;
 public class IndexMinPQ<Key extends<Comparable<Key>>>
 {
 
+     private int N; // current size of the priority queue 
+     private int maxN;
+     private Key[] keys; // array to hold keys with array index acting as Key index
+     private int[] pq; //actual priority queue array with mapping to index of each Key.
+
+     // reverse mapping between 'Key index' and 'Key position' in the Priorty Queue
+     // this is useful for checking if a given index is already to a Key or not
+     private int[] qp; // reverse mapping between 'Key index' and 'Key position' in the pri
+     
+
      public IndexMinPQ(int maxN)
      {
+         N = 0;
+         this.maxN = maxN;
+         key = new int[maxN+1];
+         pq = new int[maxN+1];
+         qp = new int[maxN+1];
+
+         for(int i = 1; i < qp.length; i++)
+         {
+            // if it is -1, then the corresponding index is not alloted to any
+            qp[i] = -1;
+         }
+     }
+
+     public void size()
+     {
+        return N;
+     }
+
+     public boolean isEmpty()
+     {
+        return (N==0);
+     }
+
+     private void validateIndex(int i)
+     {
+         if(i <= 0)
+         {
+             throw new IllegalArgumentException("given index is less than or equal to 0");
+         }
+
+         if(i > maxN)
+         {
+             throw new IllegalArgumentException("index greater than the max capacity");
+         }
+         
+     }
+
+     private boolean less(int a, int b)
+     {
+        return (keys[a].compareTo(keys[b]) < 0);
+     }
+
+     private void exch(int a, int b)
+     {
+         int swap = pq[a];
+         pq[a] = pq[b];
+         pq[b] = swap;
+
+         qp[pq[b]] = b;
+         qp[pq[a]] = a;
 
      }
 
