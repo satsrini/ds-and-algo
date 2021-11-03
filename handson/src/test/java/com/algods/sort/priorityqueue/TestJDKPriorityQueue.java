060f3af1e9533b0a9a751de140409124ed982d17
satsrini <satsrinister@gmail.com>
2021-11-03 14:41:42 +0000

    Complete JUnit for JDK Prority Queue

--- handson/src/test/java/com/algods/sort/priorityqueue/TestJDKPriorityQueue.java
+++ handson/src/test/java/com/algods/sort/priorityqueue/TestJDKPriorityQueue.java
@@ -14,8 +14,8 @@ public class TestJDKPriorityQueue
 {
 
         private PriorityQueue<String> pq;
+        private PriorityQueue<String> pq2;
         
-
 	@Before
 	public void setUp()
 	{
@@ -26,12 +26,25 @@ public class TestJDKPriorityQueue
            pq.add("zebra");
            pq.add("apple");
 
+           pq2 = new PriorityQueue<>(10,(s1,s2) -> s2.compareTo(s1));
+
+           pq2.add("Chennai");
+           pq2.add("Mumbai");
+           pq2.add("Delhi");
+           pq2.add("Kolkatta");
+
         }
 
         @Test
         public void testPriorityQueue()
         {
             System.out.println("\n\n");
+
+            for(String str:pq)
+            {
+                System.out.println(str);
+            }            
+
 
             assertEquals(Integer.valueOf(4), Integer.valueOf(pq.size()));
             assertEquals("apple", pq.poll());
@@ -44,8 +57,14 @@ public class TestJDKPriorityQueue
             assertEquals(Integer.valueOf(3), Integer.valueOf(pq.size()));
             assertEquals("ape", pq.poll());
             assertEquals(Integer.valueOf(2), Integer.valueOf(pq.size()));
+        }
 
-
+        @Test
+        public void testProrityQueue2()
+        {
+           assertEquals("Mumbai", pq2.poll());
+           assertEquals("Kolkatta", pq2.poll());
+           assertEquals(Integer.valueOf(2), Integer.valueOf(pq2.size()));
         }
 
 
