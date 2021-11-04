6071c8ebbcaba4b0654a460809336a8df1b939e3
satsrini <satsrinister@gmail.com>
2021-11-04 07:16:34 +0000

    Skeleton implementation for Eager Prim MST

---
+++ handson/src/main/java/com/algods/graph/mst/PrimMST.java
@@ -0,0 +1,28 @@
+package com.algods.graph.mst;
+
+import com.algods.graph.mst.EdgeWeightedGraph;
+
+/**
+  * <h1>PrimMST</h1>
+  * This class is an implementation for PrimMST algorithm.
+  * <p> Implementation for Prim's Minimum Spanning Tree for an Edge Weighted Undirected
+  * Graph. This approach uses Index Minimum Priority Queue of Edges with an 'eager' approac  * h.This Index Minimum Priority Queue is built using Binary Heap data structure.
+  *
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-11-04
+  */
+
+
+public class PrimMST
+{
+
+    public PrimMST(EdgeWeightedGraph g)
+    {
+
+
+    }
+
+
+}
