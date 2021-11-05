9c7f3a064e4ea9bc1c1f11e7771278e27dc25e54
satsrini <satsrinister@gmail.com>
2021-11-05 08:49:03 +0000

    quick union UnionFind

---
+++ handson/src/main/java/com/algods/graph/mst/UF2.java
@@ -0,0 +1,64 @@
+package com.algods.graph.mst;
+
+/**
+  * <h1>UF2</h1>
+  * This class is an implementation for Union Find(UF) Data Structure
+  * <p> Implementation for Union Find(UF) Data Structure in Java to be used in
+  * Kruskal's Minimum Spanning Tree algorithm.
+  * UnionFind Datastructure is used to segregate elements into various Components.
+  *
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-11-05
+  */
+
+public class UF2
+{
+
+   private int count;
+   private int[] id;
+
+   public UF2(int N)
+   {
+      count = N;
+      id = new int[N];
+   }
+
+   public int count()
+   {
+      return count;
+   }
+
+   public int find(int p)
+   {
+      while(id[p] != p)
+      {
+         p = id[p];
+      }
+
+      return p;
+   }
+
+   public boolean connected(int p, int q)
+   {
+      return find(p)==find(q); 
+   }
+
+   public void union(int p, int q)
+   {
+      int pRoot = find(p);
+      int qRoot = find(q);
+
+      if(pRoot == qRoot)
+      {
+         return;
+      }
+
+      id[pRoot] = qRoot;
+
+      count--;
+
+   }
+
+}
