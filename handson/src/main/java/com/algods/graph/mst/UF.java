e1015c57f2643b28089aa8c336b2a91870aa4d16
satsrini <satsrinister@gmail.com>
2021-11-05 05:39:29 +0000

    Skeleton implementation for Union Find data structure

---
+++ handson/src/main/java/com/algods/graph/mst/UF.java
@@ -0,0 +1,24 @@
+package com.algods.graph.mst;
+
+/**
+  * <h1>UF</h1>
+  * This class is an implementation for Union Find(UF) Data Structure
+  * <p> Implementation for Union Find(UF) Data Structure in Java to be used in
+  * Kruskal's Minimum Spanning Tree algorithm.
+  * UnionFind Datastructure is used to segregate elements into various Components.
+  *
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-11-05
+  */
+
+
+public class UF
+{
+   public UF()
+   {
+
+   }
+
+}
