a5fdfda1c8546ce921c125810bbb8e4ab75e1c27
satsrini <satsrinister@gmail.com>
2021-11-05 07:20:59 +0000

    Union Find complete implementation

--- handson/src/main/java/com/algods/graph/mst/UF.java
+++ handson/src/main/java/com/algods/graph/mst/UF.java
@@ -16,8 +16,57 @@ package com.algods.graph.mst;
 
 public class UF
 {
-   public UF()
+
+   private int[] id;
+   private int count;
+
+   public UF(int N)
    {
+     count = N;
+     
+     id = new int[N];
+
+     for(int i = 0; i < N; i++)
+     {
+        id[i] = i;
+     }
+
+   }
+
+   public int count()
+   {
+     return count;
+   }
+
+   public boolean connected(int p, int q)
+   {
+     return find(p) == find(q);
+   }
+
+   public int find(int p)
+   {
+      return id[p];
+   }
+
+   public void union(int p, int q)
+   {
+       int proot = find(p);
+       int qroot = find(q);
+
+       if(proot == qroot)
+       {
+          return;
+       }
+
+       for(int i=0; i < id.length; i++)
+       {
+          if(id[i] == proot)
+          {
+             id[i] = qroot;
+          }
+       }
+
+       count--;
 
    }
 
