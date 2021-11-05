96fb566fbe174e7bc62e8a6579df53d07de713ee
satsrini <satsrinister@gmail.com>
2021-11-05 15:33:49 +0000

    Full implementation Kruskal's Minimum Spanning tree.

--- handson/src/main/java/com/algods/graph/mst/KruskalMST.java
+++ handson/src/main/java/com/algods/graph/mst/KruskalMST.java
@@ -1,12 +1,20 @@
 package com.algods.graph.mst;
 
 import com.algods.graph.mst.EdgeWeightedGraph;
+import com.algods.graph.mst.UF;
+import com.algods.graph.mst.Edge;
+import com.algods.sort.priorityqueue.MinPQ;
+
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
   * <h1>KruskalMST</h1>
   * This class is an implementation for KruskalMST algorithm.
   * <p> Implementation for Kruskal's Minimum Spanning Tree for an Edge Weighted Undirected
-  * Graph. This approach uses Union Find Datastructure
+  * Graph. This approach uses Union Find Datastructure.
   *
   *
   * @author  Satish Srinivasan
@@ -18,10 +26,64 @@ import com.algods.graph.mst.EdgeWeightedGraph;
 public class KruskalMST
 {
 
+     private Queue<Edge> mst;
+     private MinPQ<Edge> pq;
+     private UF uf;
+
 
      public KruskalMST(EdgeWeightedGraph g)
      {
+        mst = new LinkedList<>();
+        uf = new UF(g.V());
 
+        Iterable<Edge> edgeIter = g.edges();
+
+        List<Edge> edgeList = new ArrayList<>();
+
+        Edge[] edgeArr = new Edge[g.E()];
+
+        int index = 0;
+
+        for(Edge e:edgeIter)
+        {
+           edgeArr[index++] = e;
+        }
+
+        pq = new MinPQ<>(edgeArr);      
+
+        while(!pq.isEmpty() && mst.size() < g.V()-1)
+        {
+           Edge e = pq.delMin();
+           int v = e.either();
+           int w = e.other(v);
+
+           if(uf.connected(v,w))
+           {
+              continue;
+           }
+
+           uf.union(v,w);
+
+           mst.add(e);
+
+        }
+        
      }
 
+     public Iterable<Edge> edges()
+     {
+        return mst;
+     }
+
+     public double weight()
+     {
+        double weight = 0.0;
+
+        for(Edge e:mst)
+        {
+          double += e.weight();
+        }
+
+        return weight;
+     }
 }
