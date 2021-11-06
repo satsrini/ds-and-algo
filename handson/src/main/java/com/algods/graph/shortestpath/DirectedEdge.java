229529a833ad541da6cbf88b5239111272d1dd51
satsrini <satsrinister@gmail.com>
2021-11-06 15:16:31 +0000

    Skeleton implementation for Directed Edge Data Structure.

---
+++ handson/src/main/java/com/algods/graph/shortestpath/DirectedEdge.java
@@ -0,0 +1,24 @@
+package com.algods.graph.shortestpath;
+
+
+/**
+  * <h1>DirectedEdge</h1>
+  * This class is an implementation for Directed Edge Data Structure
+  * <p> Implementation for Directed Edge Data Structure in Java to be used in
+  * finding shortest paths in an Edge Weighted Directed Graph.
+  *
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-11-06
+  */
+
+
+public class DirectedEdge
+{
+    public DirectedEdge()
+    {
+
+    }
+
+}
