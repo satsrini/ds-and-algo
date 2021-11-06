b2a6ccdc48ed9aebd807ef18964785a23a40816a
satsrini <satsrinister@gmail.com>
2021-11-06 16:01:00 +0000

    Skeleton Implementation for Edge Weighted Directed Graph.

---
+++ handson/src/main/java/com/algods/graph/shortestpath/EdgeWeightedDiGraph.java
@@ -0,0 +1,24 @@
+package com.algods.graph.shortestpath;
+
+/**
+  * <h1>EdgeWeightedDiGraph</h1>
+  * This class is an implementation for Edge Weighted Directed Graph Data Structure
+  * <p> Implementation for Edge Weighted Directed Graph Data Structure in Java to be used
+  * in finding shortest paths.
+  *
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-11-06
+  */
+
+
+public class EdgeWeightedDiGraph
+{
+
+   public EdgeWeightedDiGraph()
+   {
+
+   }
+
+}
