94e7784bf0ae21b24c643b6ac58f3e961296c0ec
satsrini <satsrinister@gmail.com>
2021-11-07 12:35:46 +0000

    Skeleton implementation for DijkstraSP Algorithm

---
+++ handson/src/main/java/com/algods/graph/shortestpath/DijkstraSP.java
@@ -0,0 +1,24 @@
+package com.algods.graph.shortestpath;
+
+/**
+  * <h1>DijkstraSP</h1>
+  * This class is an implementation for DijkstraSP Algorithm
+  * <p> Implementation for DijkstraSP Algorithm in Java to be used
+  * in finding shortest paths.
+  *
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-11-07
+  */
+
+
+public class DijkstraSP
+{
+
+   public DijkstraSP()
+   {
+
+   }
+
+}
