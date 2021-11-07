9cdb9df8f763fd3c7e212775ea9248a5a927b7ca
satsrini <satsrinister@gmail.com>
2021-11-07 12:52:36 +0000

    Some more implementation of DijkstraSP Algorithm

--- handson/src/main/java/com/algods/graph/shortestpath/DijkstraSP.java
+++ handson/src/main/java/com/algods/graph/shortestpath/DijkstraSP.java
@@ -27,6 +27,24 @@ public class DijkstraSP
       edgeTo = new DirectedEdge[g.V()];
       distTo = new double[g.V()];
       pq = new IndexMinPQ<>(g.V());
+
+      for(int i = 0; i < distTo.length; i++)
+      {
+         distTo[i] = Double.POSITIVE_INFINITY;
+      }
+
+      pq.insert(s,0.0); // here source and dest are both s, hence weight = 0.0
+
+      while(!pq.isEmpty())
+      {
+        relax(g,pq.delMin());
+      }
+
+   }
+
+   private void relax(EdgeWeightedDiGraph g, int v)
+   {
+
    }
 
 }
