739ac25641062bcbd8598910b336648727deab04
satsrini <satsrinister@gmail.com>
2021-11-11 16:13:56 +0000

    Skeleton implementation of Depth First Order for an Edge Weighted Directed Graph.

---
+++ handson/src/main/java/com/algods/graph/shortestpath/acyclic/DepthFirstOrderEdgeWeighted.java
@@ -0,0 +1,27 @@
+package com.algods.graph.shortestpath.acyclic;
+
+import com.algods.graph.shortestpath.EdgeWeightedDiGraph;
+
+/**
+  * <h1>DepthFirstOrderEdgeWeighted</h1>
+  * This class is an implementation for DepthFirstOrderEdgeWeighted Algorithm.
+  * <p> Implementation DepthFirstOrderEdgeWeighted in Java to be used to find 
+  *     preorder, postorder and reverse post order of elements in an Edge Weighted
+  *     Directed Graph. The reverse post order would give Topological order of the 
+  *     elements in the given Edge Weighted Directed Graph.
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-11-11
+  */
+
+
+public class DepthFirstOrderEdgeWeighted
+{
+
+    public DepthFirstOrderEdgeWeighted(EdgeWeightedDiGraph g)
+    {
+
+    }
+
+}
