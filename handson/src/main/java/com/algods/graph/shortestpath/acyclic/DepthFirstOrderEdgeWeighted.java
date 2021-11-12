3628c98e058e9ce1296f7c7efb00519a5cf7c76d
satsrini <satsrinister@gmail.com>
2021-11-12 18:10:48 +0000

    Complete Implementation of Depth First Order for an Edge Weighted Directed Graph

--- handson/src/main/java/com/algods/graph/shortestpath/acyclic/DepthFirstOrderEdgeWeighted.java
+++ handson/src/main/java/com/algods/graph/shortestpath/acyclic/DepthFirstOrderEdgeWeighted.java
@@ -1,14 +1,20 @@
 package com.algods.graph.shortestpath.acyclic;
 
 import com.algods.graph.shortestpath.EdgeWeightedDiGraph;
+import com.algods.graph.shortestpath.DirectedEdge;
+
+import java.util.Queue;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.ArrayDeque;
 
 /**
   * <h1>DepthFirstOrderEdgeWeighted</h1>
   * This class is an implementation for DepthFirstOrderEdgeWeighted Algorithm.
-  * <p> Implementation DepthFirstOrderEdgeWeighted in Java to be used to find 
-  *     preorder, postorder and reverse post order of elements in an Edge Weighted
-  *     Directed Graph. The reverse post order would give Topological order of the 
-  *     elements in the given Edge Weighted Directed Graph.
+  * <p> Implementation of DepthFirstOrderEdgeWeighted in Java to be used to find 
+  *     preorder, postorder and reverse post order of Directed Edges in an Edge Weighted
+  *     Directed Graph. The reverse post order would give the Topological order of the 
+  *     Directed Edges in the given Edge Weighted Directed Graph.
   *
   * @author  Satish Srinivasan
   * @version 1.0
@@ -19,9 +25,51 @@ import com.algods.graph.shortestpath.EdgeWeightedDiGraph;
 public class DepthFirstOrderEdgeWeighted
 {
 
+    private boolean[] marked;
+    private Queue<Integer> preOrder;
+    private Queue<Integer> postOrder;
+    private Deque<Integer> reversePostOrder;
+   
     public DepthFirstOrderEdgeWeighted(EdgeWeightedDiGraph g)
     {
+       marked = new boolean[g.V()];
+       preOrder = new LinkedList<>();
+       postOrder = new LinkedList<>();
+       reversePostOrder = new ArrayDeque<>();
+    }
+
+    private void dfs(EdgeWeightedDiGraph g, int v)
+    {
+       marked[v] = true;
+
+       preOrder.add(v);
+       for(DirectedEdge e:g.adj(v))
+       {
+          int w = e.to();
+
+          if(!marked[w])
+          {
+             dfs(g,w);
+          }
+
+       }
+       postOrder.add(v);
+       reversePostOrder.addFirst(v); // addFirst() makes it a stack during Iteration.
 
     }
 
+    public Iterable<Integer> pre()
+    {
+       return preOrder;
+    }
+
+    public Iterable<Integer> post()
+    {
+       return postOrder;
+    }
+
+    public Iterable<Integer> reversePost()
+    {
+       return reversePostOrder;
+    }
 }
