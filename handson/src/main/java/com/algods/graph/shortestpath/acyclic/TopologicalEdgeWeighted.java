52be97e5fd0b554b321ae17c24c158733962c6e2
satsrini <satsrinister@gmail.com>
2021-11-13 06:56:41 +0000

    Skeleton implementation of Topological for an Edge Weighted DAG

---
+++ handson/src/main/java/com/algods/graph/shortestpath/acyclic/TopologicalEdgeWeighted.java
@@ -0,0 +1,25 @@
+package com.algods.graph.shortestpath.acyclic;
+
+import com.algods.graph.shortestpath.EdgeWeightedDiGraph;
+
+/**
+  * <h1>TopologicalEdgeWeighted</h1>
+  * This class is an implementation for TopologicalEdgeWeighted Algorithm.
+  * <p> Implementation of TopologicalEdgeWeighted Algorithm in Java to be used to find
+  *   Topologocal order in an Edge Weighted Directed Acyclic Graph(DAG).
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-11-13
+  */
+
+
+public class TopologicalEdgeWeighted
+{
+
+   public TopologicalEdgeWeighted(EdgeWeightedDiGraph g)
+   {
+
+   }
+
+}
