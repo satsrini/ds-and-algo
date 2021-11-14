05293a4705cd3d745e2e40536eeedc5a140b3e14
satsrini <satsrinister@gmail.com>
2021-11-14 09:51:05 +0000

    JNUnit for AcyclicLP - Longest Path for Edge Weighted DAG

---
+++ handson/src/test/java/com/algods/graph/shortestpath/acyclic/TestAcyclicLP.java
@@ -0,0 +1,287 @@
+package com.algods.graph.shortestpath.acyclic;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.algods.graph.shortestpath.EdgeWeightedDiGraph;
+import com.algods.graph.shortestpath.DirectedEdge;
+import com.algods.graph.shortestpath.acyclic.AcyclicLP;
+
+
+public class TestAcyclicLP
+{
+
+        private EdgeWeightedDiGraph g1;
+
+
+	@Before
+	public void setUp()
+	{
+            g1 = getAcyclic();
+        }
+
+        @Test
+        public void testAcylicLPPath()
+        {
+            AcyclicLP acyclicLP = new AcyclicLP(g1,5);
+
+            assertTrue(acyclicLP.hasPathTo(6));
+
+            AcyclicLP acyclicLP2 = new AcyclicLP(g1,0);   
+            //assertFalse(acyclicLP2.hasPathTo(1));
+        }
+
+        @Test
+        public void testAcylicLPPath2()
+        {
+            AcyclicLP acyclicLP = new AcyclicLP(g1,5);
+
+            System.out.println("\n\n");
+            System.out.println("path to 6 from 5:");
+            for(DirectedEdge e:acyclicLP.pathTo(6))
+            {
+                System.out.println(e);
+            }
+            System.out.println("\n\n");
+
+            assertEquals(true,true);
+        }
+
+        @Test
+        public void testAcylicLPPath3()
+        {
+            AcyclicLP acyclicLP = new AcyclicLP(g1,0);
+
+            System.out.println("\n\n");
+            System.out.println("path to 2 from 0:");
+            for(DirectedEdge e:acyclicLP.pathTo(2))
+            {
+                System.out.println(e);
+            }
+            System.out.println("\n\n");
+
+            assertEquals(true,true);
+        }
+
+        @Test
+        public void testAcylicLPPath4()
+        {
+            AcyclicLP acyclicLP4 = new AcyclicLP(getAcyclic2(),0);
+
+            System.out.println("\n\n");
+            System.out.println("path to 2 from 0 with negative weight:");
+            for(DirectedEdge e:acyclicLP4.pathTo(2))
+            {
+                System.out.println(e);
+            }
+            System.out.println("\n\n");
+
+            assertEquals(true,true);
+        }
+
+
+        @Test
+        public void testAcylicLPPath5()
+        {
+            AcyclicLP acyclicLP5 = new AcyclicLP(getAcyclic2(),1);
+
+            System.out.println("\n\n");
+            System.out.println("path to 6 from 1 with negative weight:");
+            for(DirectedEdge e:acyclicLP5.pathTo(6))
+            {
+                System.out.println(e);
+            }
+            System.out.println("\n\n");
+
+            assertEquals(true,true);
+        }
+
+        @Test
+        public void testAcylicLPPath6()
+        {
+            AcyclicLP acyclicLP = new AcyclicLP(g1,5);
+
+            System.out.println("\n\n");
+            System.out.println("path to 2 from 5 with positive weight:");
+            for(DirectedEdge e:acyclicLP.pathTo(2))
+            {
+                System.out.println(e);
+            }
+            System.out.println("\n\n");
+
+            AcyclicLP acyclicLP2 = new AcyclicLP(getAcyclic2(),5);
+
+            System.out.println("\n\n");
+            System.out.println("path to 2 from 5 with negative weight:");
+            for(DirectedEdge e:acyclicLP2.pathTo(2))
+            {
+                System.out.println(e);
+            }
+            System.out.println("\n\n");
+
+
+            assertEquals(true,true);
+        }
+
+        @Test
+        public void testAcylicLPPath7()
+        {
+            AcyclicLP acyclicLP = new AcyclicLP(g1,5);
+
+            System.out.println("\n\n");
+            System.out.println("path to 0 from 5 with positive weight:");
+            for(DirectedEdge e:acyclicLP.pathTo(0))
+            {
+                System.out.println(e);
+            }
+            System.out.println("\n\n");
+
+            AcyclicLP acyclicLP2 = new AcyclicLP(getAcyclic2(),5);
+
+            System.out.println("\n\n");
+            System.out.println("path to 0 from 5 with negative weight:");
+            for(DirectedEdge e:acyclicLP2.pathTo(0))
+            {
+                System.out.println(e);
+            }
+            System.out.println("\n\n");
+
+
+            assertEquals(true,true);
+
+
+
+        }
+
+        @Test
+        public void testAcylicLPPath9()
+        {
+            AcyclicLP acyclicLP = new AcyclicLP(g1,5);
+
+            System.out.println("\n\n");
+            System.out.println("path to 2 from 5 with positive weight:");
+            for(DirectedEdge e:acyclicLP.pathTo(2))
+            {
+                System.out.println(e);
+            }
+            System.out.println("\n\n");
+
+            AcyclicLP acyclicLP3 = new AcyclicLP(getAcyclic3(),5);
+
+            System.out.println("\n\n");
+            System.out.println("path to 2 from 5 with double negative weight:");
+            for(DirectedEdge e:acyclicLP3.pathTo(2))
+            {
+                System.out.println(e);
+            }
+            System.out.println("\n\n");
+
+
+            assertEquals(true,true);
+
+
+
+        }
+
+
+        @Test
+        public void testAcylicLPPath8()
+        {
+            AcyclicLP acyclicLP = new AcyclicLP(g1,0);
+
+            System.out.println("\n\n");
+            System.out.println("THIS IS WRONG: path to 1 from 0 with positive weight:");
+            for(DirectedEdge e:acyclicLP.pathTo(1))
+            {
+                System.out.println(e);
+            }
+            System.out.println("\n\n");
+
+
+            assertEquals(true,true);
+
+
+
+        }
+
+
+
+
+        private EdgeWeightedDiGraph getAcyclic()
+        {
+
+           EdgeWeightedDiGraph g = new EdgeWeightedDiGraph(8);
+
+           g.addEdge(new DirectedEdge(5,4,0.35));
+           g.addEdge(new DirectedEdge(4,7,0.37));
+           g.addEdge(new DirectedEdge(5,7,0.28));
+           g.addEdge(new DirectedEdge(5,1,0.32));
+           g.addEdge(new DirectedEdge(4,0,0.38));
+           g.addEdge(new DirectedEdge(0,2,0.26));
+           g.addEdge(new DirectedEdge(3,7,0.39));
+           g.addEdge(new DirectedEdge(1,3,0.29));
+           g.addEdge(new DirectedEdge(7,2,0.34));
+           g.addEdge(new DirectedEdge(6,2,0.40));
+           g.addEdge(new DirectedEdge(3,6,0.52));
+           g.addEdge(new DirectedEdge(6,0,0.58));
+           g.addEdge(new DirectedEdge(6,4,0.93));
+
+
+           return g;
+
+        }
+
+        private EdgeWeightedDiGraph getAcyclic2()
+        {
+
+           EdgeWeightedDiGraph g = new EdgeWeightedDiGraph(8);
+
+           g.addEdge(new DirectedEdge(5,4,0.35));
+           g.addEdge(new DirectedEdge(4,7,0.37));
+           g.addEdge(new DirectedEdge(5,7,0.28));
+           g.addEdge(new DirectedEdge(5,1,0.32));
+           g.addEdge(new DirectedEdge(4,0,-0.65)); // negative weight
+           g.addEdge(new DirectedEdge(0,2,0.26));
+           g.addEdge(new DirectedEdge(3,7,0.39));
+           g.addEdge(new DirectedEdge(1,3,0.29));
+           g.addEdge(new DirectedEdge(7,2,0.34));
+           g.addEdge(new DirectedEdge(6,2,0.40));
+           g.addEdge(new DirectedEdge(3,6,0.52));
+           g.addEdge(new DirectedEdge(6,0,0.58));
+           g.addEdge(new DirectedEdge(6,4,0.93));
+
+
+           return g;
+
+        }
+
+        private EdgeWeightedDiGraph getAcyclic3()
+        {
+
+           EdgeWeightedDiGraph g = new EdgeWeightedDiGraph(8);
+
+           g.addEdge(new DirectedEdge(5,4,0.35));
+           g.addEdge(new DirectedEdge(4,7,-2.50)); // negative weight
+           g.addEdge(new DirectedEdge(5,7,0.28));
+           g.addEdge(new DirectedEdge(5,1,0.32));
+           g.addEdge(new DirectedEdge(4,0,-0.65)); // negative weight
+           g.addEdge(new DirectedEdge(0,2,0.26));
+           g.addEdge(new DirectedEdge(3,7,0.39));
+           g.addEdge(new DirectedEdge(1,3,0.29));
+           g.addEdge(new DirectedEdge(7,2,0.34));
+           g.addEdge(new DirectedEdge(6,2,0.40));
+           g.addEdge(new DirectedEdge(3,6,0.52));
+           g.addEdge(new DirectedEdge(6,0,0.58));
+           g.addEdge(new DirectedEdge(6,4,0.93));
+
+
+           return g;
+
+        }
+
+
+}
