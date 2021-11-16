e78c5355a5da53c0d90e5a3ee64e39788fab0f4d
satsrini <satsrinister@gmail.com>
2021-11-16 07:27:03 +0000

    Skeleton implementationg for LSD sorting for Strings

---
+++ handson/src/main/java/com/algods/strings/sort/LSD.java
@@ -0,0 +1,26 @@
+package com.algods.strings.sort;
+
+
+/**
+  * <h1>LSD</h1>
+  * This class is an implementation for Least Significant Digits(LSD) algorithm for strings
+  * <p> Implementation for LSD algorithm for Strings in Java.
+  *     It uses key Indexed Counting technique to sort given list of Strings with equal
+  *     length. Or to sort based on leading no of characters say W no of characters.
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-11-16
+  */
+
+
+public class LSD
+{
+
+    public LSD(String[] a, int W)
+    {
+
+    }
+    
+
+}
