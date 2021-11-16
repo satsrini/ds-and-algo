37df96a967220920ca6a61934512e7799d2d8f7a
satsrini <satsrinister@gmail.com>
2021-11-16 16:42:21 +0000

    Complete implementation of LSD - Least Significant Digits algorithm for sorting an array of Strings.

--- handson/src/main/java/com/algods/strings/sort/LSD.java
+++ handson/src/main/java/com/algods/strings/sort/LSD.java
@@ -17,10 +17,39 @@ package com.algods.strings.sort;
 public class LSD
 {
 
-    public LSD(String[] a, int W)
+    public LSD(){}
+
+    public void sort(String[] a, int W)
     {
 
+      int R = 256;
+      String[] aux = new String[a.length];
+
+      for(int d = W-1; d >= 0; d--)
+      {
+
+          int[] count = new int[R+1];
+
+          for(int k = 0; k < a.length;k++)
+          {
+               count[a[k].charAt(d)+1]++;
+          }
+
+          for(int k = 0; k < R; k++)
+          {
+               count[k+1] += count[k];
+          }
+
+          for(int k = 0; k < a.length; k++)
+          {
+               aux[count[a[k].charAt(d)]++] = a[k];              
+          }
+
+          for(int k = 0; k < a.length; k++)
+          {
+               a[k] = aux[k];
+          }
+      }
     }
-    
 
 }
