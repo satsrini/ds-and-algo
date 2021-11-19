6b3fb5c9fbab525129b98d74dada1a674e237872
satsrini <satsrinister@gmail.com>
2021-11-19 17:24:38 +0000

    Skeleton implementation of Quick3String

---
+++ handson/src/main/java/com/algods/strings/sort/Quick3String.java
@@ -0,0 +1,32 @@
+package com.algods.strings.sort;
+
+
+/**
+  * <h1>Quick3String</h1>
+  * This class is an implementation for Quick3String algorithm
+  * <p> Implementation of Quick3String algorithm which sorts an array
+  *     of Strings by recursively dividing the array and subarrays into
+  *     3 parts.
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-11-19
+  */
+
+
+public class Quick3String
+{
+
+    private String[] a;
+
+    public Quick3String(String[] a)
+    {
+       this.a = a;
+    }
+
+    public void sort()
+    {
+
+    }
+
+}
