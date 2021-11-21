95022f3ed36081f8cab3fec8e927e8095af0151b
satsrini <satsrinister@gmail.com>
2021-11-21 19:36:31 +0000

    Skeleton implementation for TrieST

---
+++ handson/src/main/java/com/algods/strings/search/TrieST.java
@@ -0,0 +1,37 @@
+package com.algods.strings.search;
+
+/**
+  * <h1>TrieST</h1>
+  * This class is an implementation for TrieST Data Structure
+  * <p> Implementation for TrieST data Strcuture in java.
+  *     This symbol table has Key as Strings and it is used to search String Keys including wild card seraches.
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-11-21
+  */
+
+
+
+public class TrieST<Value>
+{
+
+    private static int R = 256; // all ASCII characters
+
+    private Node root;
+
+    public TrieST()
+    {
+
+    }
+
+    private static class Node
+    {
+       private Object value;
+
+       private Node[] children = new Node[R];
+       
+    }
+
+
+}
