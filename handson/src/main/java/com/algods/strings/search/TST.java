ea83f30ca7b7e6cee41dcfd6dc47437eaf1fccb0
satsrini <satsrinister@gmail.com>
2021-11-27 19:37:10 +0000

    Complete Implementation of get and put for Ternary Search Tries(TST) data structure

--- handson/src/main/java/com/algods/strings/search/TST.java
+++ handson/src/main/java/com/algods/strings/search/TST.java
@@ -18,6 +18,9 @@ package com.algods.strings.search;
 
 public class TST<Value>
 {
+
+   private Node root;
+
    private class Node
    {
       private char c;
@@ -34,13 +37,76 @@ public class TST<Value>
 
    public Value get(String key)
    {
-      return null;
+
+      Node x = get(root, key, 0);
+
+      if(x == null)
+      {
+         return null;
+      }
+
+      return x.val;
    }
 
+   private Node get(Node x, String key, int d)
+   {
 
+      if(x == null)
+      {
+         return null;
+      }
+
+      char c = key.charAt(d);
+
+      if(c < x.c)
+      {
+        x = get(x.left,key,d); // go left
+      }else
+      if(c > x.c)    // go right
+      {
+        x = get(x.right,key,d);
+      }else   // match for the character at position 'd' in the given 'key'
+      if(d < key.length() -1) // go down next level.
+      {
+        x = get(x.mid, key, d+1);
+      }
+      
+      return x;
+      
+   }
 
    public void put(String key, Value val)
    {
+      root = put(root, key, val, 0);
+   }
+
+   private Node put(Node x, String key, Value val, int d)
+   {
+
+      char c = key.charAt(d);
+
+      if(x == null)
+      {
+         x = new Node();
+         x.c = c;
+      }          
+
+      if(c < x.c) // go left
+      {
+         x.left = put(x.left, key, val, d);
+      }else
+      if(c > x.c) // go right
+      {
+         x.right = put(x.right, key, val, d);
+      }else
+      if(d < key.length()-1)
+      {
+         x.mid = put(x.mid, key, val, d+1);
+      }
+      
+      x.val = val;
+
+      return x;
 
    }
 
