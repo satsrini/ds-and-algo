7fde8ad22055907d61c0d59557ec9cc4757cad9a
satsrini <satsrinister@gmail.com>
2021-11-28 16:47:15 +0000

    Skeleton implementation of Brute Force method for substring String search

---
+++ handson/src/main/java/com/algods/strings/substringsearch/BruteForce.java
@@ -0,0 +1,32 @@
+package com.algods.strings.substringsearch;
+
+/**
+  * <h1>BruteForce</h1>
+  * This class is an implementation of BruteForce
+  * <p> Implementation of BruteForce method for substring search in Java
+  *
+  *
+  * @author  Satish Srinivasan
+  * @version 1.0
+  * @since   2021-11-28
+  */
+
+
+
+public class BruteForce
+{
+   public BruteForce()
+   {
+
+   }
+
+   public int search(String text, String pat)
+   {
+      int result = -1;
+
+
+      return result;
+
+   }
+
+}
